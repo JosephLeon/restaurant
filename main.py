6b8baee568f918bd39b04101492a33bd9ffb46ab
Joseph Leon <joseph@poeticsystems.com>
2014-10-30 14:47:21 +0200

    Add kv file for entity manipulation, add user input detail.

--- main.py
+++ main.py
@@ -10,17 +10,17 @@ class GetUserInput(GridLayout):
     def __init__(self, **kwargs):
         super(GetUserInput, self).__init__(**kwargs)
         self.cols = 2
-        self.add_widget(Label(text='User Name'))
-        self.username = TextInput(multiline=False)
-        self.add_widget(self.username)
-        self.add_widget(Label(text='password'))
-        self.password = TextInput(password=True, multiline=False)
-        self.add_widget(self.password)
+        self.add_widget(Label(text='On a scale of 1 to 5 how would you rate the service_rating?'))
+        self.service_rating = TextInput(multiline=False)
+        self.add_widget(self.service_rating)
+        self.add_widget(Label(text='On a scale of 1 to 5 how would you rate the food?'))
+        self.food_rating = TextInput(multiline=False)
+        self.add_widget(self.food_rating)
 
-class RestaurantExplorer(App):
+class RestaurantMate(App):
     def build(self):
         return GetUserInput()
 
 
 if __name__ == '__main__':
-    RestaurantExplorer().run()
+    RestaurantMate().run()
