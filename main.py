0ec6261b10f0d8ea0f9494dce4c23b08e06fa20b
Joseph Leon <joseph@poeticsystems.com>
2014-10-31 14:36:11 +0200

    Experiment with boxlayout orientation.

--- main.py
+++ main.py
@@ -5,13 +5,19 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.uix.anchorlayout import AnchorLayout
 
 class GetUserInput(BoxLayout):
     def __init__(self, **kwargs):
         super(GetUserInput, self).__init__(**kwargs)
-        # self.cols = 2
+        # layout = BoxLayout(orientation='vertical')
+        # btn1 = Button(text='Hello')
+        # btn2 = Button(text='World')
+        # layout.add_widget(btn1)
+        # layout.add_widget(btn2)
+        self.orientation = 'vertical'
         self.size_hint = (1.0, None)
-        # self.width = 250
         self.height = 30
         self.add_widget(Label(text='On a scale of 1 to 5 how would you rate the service_rating?'))
         self.service_rating = TextInput(multiline=False)
@@ -19,6 +25,7 @@ class GetUserInput(BoxLayout):
         self.add_widget(Label(text='On a scale of 1 to 5 how would you rate the food?'))
         self.food_rating = TextInput(multiline=False)
         self.add_widget(self.food_rating)
+        self.add_widget(Button(text='Submit'))
 
 class RestaurantMate(App):
     def build(self):
