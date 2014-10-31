d61236023c6b977d3027b2a8443ed06afa0f2e43
Joseph Leon <joseph@poeticsystems.com>
2014-10-31 13:46:01 +0200

    Switch layout to boxlayout and adjust user input dimensions.

--- main.py
+++ main.py
@@ -2,14 +2,17 @@ import kivy
 kivy.require('1.8.0')
 
 from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 
-class GetUserInput(GridLayout):
+class GetUserInput(BoxLayout):
     def __init__(self, **kwargs):
         super(GetUserInput, self).__init__(**kwargs)
-        self.cols = 2
+        # self.cols = 2
+        self.size_hint = (1.0, None)
+        # self.width = 250
+        self.height = 30
         self.add_widget(Label(text='On a scale of 1 to 5 how would you rate the service_rating?'))
         self.service_rating = TextInput(multiline=False)
         self.add_widget(self.service_rating)
