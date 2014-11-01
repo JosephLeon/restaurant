e70cf660532d12f5946ae1954d1a54790c87078c
Joseph Leon <joseph@poeticsystems.com>
2014-11-01 20:06:53 +0200

    Remove graphics code, cleanup commented code.

--- main.py
+++ main.py
@@ -30,43 +30,19 @@ from kivy.graphics.instructions import InstructionGroup
     #     self.add_widget(self.food_rating)
     #     self.add_widget(Button(text='Submit'))
 
-class Graphics(Widget):
-
-    def build_graphics(self):
-        self = InstructionGroup()
-
-        pass
-
 class RestaurantMate(App):
     def build(self):
         controls = AnchorLayout(
             anchor_x='right',
             anchor_y='top',
-            # border=
         )
         box = BoxLayout(
             orientation='vertical'
         )
-        # box = BoxLayout(size_hint_y=None, orientation='vertical')
-
-        # background = Rectangle(
-        #     pos=self.pos,
-        #     size=self.size
-        # )
-
-        # background = build_graphics()
-
-        # blue = InstructionGroup()
-        # blue.add(Color(255, 255, 255, 1.0))
-        # blue.add(Rectangle(size=(100, 100)))
-        # pos=self.pos, size=(100%, 100)
 
         service_rating = Label(
             text='On a scale of 1 to 5 how would you rate the service?',
             font_size=18,
-            size_hint_x=None,
-            multiline=True,
-            # width=100,
         )
         service_rating_entry = TextInput(
             font_size=18,
@@ -76,31 +52,23 @@ class RestaurantMate(App):
         food_rating = Label(
             text='On a scale of 1 to 5 how would you rate the food?',
             font_size=18,
-            size_hint_x=None,
-            multiline=True,
         )
         food_rating_entry = TextInput(
             font_size=18,
             size_hint_x=None,
             width=200
         )
-        input_btn = Button(text='Input', font_size=40, size_hint_x=None,
+        input_btn = Button(text='Calculate Tip', font_size=40, size_hint_x=None,
                           width=200, background_color=[0,1.7,0,1])
 
-        # controls.canvas.add(blue)
-        # controls.canvas.add(background)
         controls.add_widget(box)
         box.add_widget(service_rating)
         box.add_widget(service_rating_entry)
         box.add_widget(food_rating)
         box.add_widget(food_rating_entry)
         box.add_widget(input_btn)
-        # Graphics()
 
         return controls
-        return Graphics()
-        # return GetUserInput()
-
 
 if __name__ == '__main__':
     RestaurantMate().run()
