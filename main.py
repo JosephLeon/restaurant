774dac504bf0b7e6681359233755b6bccbf3c99a
Joseph Leon <joseph@poeticsystems.com>
2014-11-01 20:36:19 +0200

    Include food cost input, wrap up all initial user input.

--- main.py
+++ main.py
@@ -11,25 +11,6 @@ from kivy.uix.anchorlayout import AnchorLayout
 from kivy.graphics import Color, Rectangle, Line
 from kivy.graphics.instructions import InstructionGroup
 
-# class GetUserInput(BoxLayout):
-    # def __init__(self, **kwargs):
-    #     super(GetUserInput, self).__init__(**kwargs)
-    #     # layout = BoxLayout(orientation='vertical')
-    #     # btn1 = Button(text='Hello')
-    #     # btn2 = Button(text='World')
-    #     # layout.add_widget(btn1)
-    #     # layout.add_widget(btn2)
-    #     self.orientation = 'vertical'
-    #     self.size_hint = (1.0, None)
-    #     self.height = 30
-    #     self.add_widget(Label(text='On a scale of 1 to 5 how would you rate the service_rating?'))
-    #     self.service_rating = TextInput(multiline=False)
-    #     self.add_widget(self.service_rating)
-    #     self.add_widget(Label(text='On a scale of 1 to 5 how would you rate the food?'))
-    #     self.food_rating = TextInput(multiline=False)
-    #     self.add_widget(self.food_rating)
-    #     self.add_widget(Button(text='Submit'))
-
 class RestaurantMate(App):
     def build(self):
         controls = AnchorLayout(
@@ -40,20 +21,29 @@ class RestaurantMate(App):
             orientation='vertical'
         )
 
-        service_rating = Label(
+        service_rating_label = Label(
             text='On a scale of 1 to 5 how would you rate the service?',
             font_size=18,
         )
-        service_rating_entry = TextInput(
+        service_rating = TextInput(
             font_size=18,
             size_hint_x=None,
             width=200
         )
-        food_rating = Label(
+        food_rating_label = Label(
             text='On a scale of 1 to 5 how would you rate the food?',
             font_size=18,
         )
-        food_rating_entry = TextInput(
+        food_rating = TextInput(
+            font_size=18,
+            size_hint_x=None,
+            width=200
+        )
+        food_cost_label = Label(
+            text='On a scale of 1 to 5 how would you rate the food?',
+            font_size=18,
+        )
+        food_cost = TextInput(
             font_size=18,
             size_hint_x=None,
             width=200
@@ -62,10 +52,12 @@ class RestaurantMate(App):
                           width=200, background_color=[0,1.7,0,1])
 
         controls.add_widget(box)
+        box.add_widget(service_rating_label)
         box.add_widget(service_rating)
-        box.add_widget(service_rating_entry)
+        box.add_widget(food_rating_label)
         box.add_widget(food_rating)
-        box.add_widget(food_rating_entry)
+        box.add_widget(food_cost_label)
+        box.add_widget(food_cost)
         box.add_widget(input_btn)
 
         return controls
