0843934b7374312d0a2c37087cb14fc13c0fe34a
Joseph Leon <joseph@poeticsystems.com>
2014-11-01 19:46:29 +0200

    Test various graphic output.

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import kivy
 kivy.require('1.8.0')
 
 from kivy.app import App
+from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.anchorlayout import AnchorLayout
+from kivy.graphics import Color, Rectangle, Line
+from kivy.graphics.instructions import InstructionGroup
 
 # class GetUserInput(BoxLayout):
     # def __init__(self, **kwargs):
@@ -27,31 +30,75 @@ from kivy.uix.anchorlayout import AnchorLayout
     #     self.add_widget(self.food_rating)
     #     self.add_widget(Button(text='Submit'))
 
+class Graphics(Widget):
+
+    def build_graphics(self):
+        self = InstructionGroup()
+
+        pass
+
 class RestaurantMate(App):
     def build(self):
-        controls = AnchorLayout(anchor_x='right', anchor_y='top', height=200)
-        box = BoxLayout(size_hint_y=None, height=150, orientation='vertical')
+        controls = AnchorLayout(
+            anchor_x='right',
+            anchor_y='top',
+            # border=
+        )
+        box = BoxLayout(
+            orientation='vertical'
+        )
+        # box = BoxLayout(size_hint_y=None, orientation='vertical')
 
-        upc_l = Label(
+        # background = Rectangle(
+        #     pos=self.pos,
+        #     size=self.size
+        # )
+
+        # background = build_graphics()
+
+        # blue = InstructionGroup()
+        # blue.add(Color(255, 255, 255, 1.0))
+        # blue.add(Rectangle(size=(100, 100)))
+        # pos=self.pos, size=(100%, 100)
+
+        service_rating = Label(
             text='On a scale of 1 to 5 how would you rate the service?',
             font_size=18,
             size_hint_x=None,
+            multiline=True,
             # width=100,
         )
-        entry = TextInput(
-            font_size=40,
+        service_rating_entry = TextInput(
+            font_size=18,
             size_hint_x=None,
-            width=350
+            width=200
         )
-        search_b = Button(text='Input', font_size=40, size_hint_x=None,
+        food_rating = Label(
+            text='On a scale of 1 to 5 how would you rate the food?',
+            font_size=18,
+            size_hint_x=None,
+            multiline=True,
+        )
+        food_rating_entry = TextInput(
+            font_size=18,
+            size_hint_x=None,
+            width=200
+        )
+        input_btn = Button(text='Input', font_size=40, size_hint_x=None,
                           width=200, background_color=[0,1.7,0,1])
 
+        # controls.canvas.add(blue)
+        # controls.canvas.add(background)
         controls.add_widget(box)
-        box.add_widget(upc_l)
-        box.add_widget(entry)
-        box.add_widget(search_b)
+        box.add_widget(service_rating)
+        box.add_widget(service_rating_entry)
+        box.add_widget(food_rating)
+        box.add_widget(food_rating_entry)
+        box.add_widget(input_btn)
+        # Graphics()
 
         return controls
+        return Graphics()
         # return GetUserInput()
 
 
